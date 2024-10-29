a7618a555d7c2edd3426e1ad46fdbf8b684f3d2c
HehNice <122663831+HehNiceArt@users.noreply.github.com>
2024-10-29 22:36:26 +0800

    just minor line fix

--- Assets/Scripts/Player/PlayerWallRide.cs
+++ Assets/Scripts/Player/PlayerWallRide.cs
@@ -24,7 +24,6 @@ public class PlayerWallRide : MonoBehaviour
         {
             if (!wasTouchingWall)
             {
-
                 wallStickTimer = maxWallStickTime;
             }
             wallStickTimer -= Time.deltaTime;
